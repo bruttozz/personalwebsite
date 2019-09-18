53a61cefe34bd6c58ab66523075c773a7de7db7f
brutto <hongtaoz@bu.edu>
2019-09-18 11:46:50 -0400

    initail commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+// Setup express static public folder for CSS, JS and images
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    // res.send('Welcome to Brutto/s personal website!');
+    res.render('home');
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
